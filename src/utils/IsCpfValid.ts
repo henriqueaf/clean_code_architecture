f7fd61c047e591383811528a77a3d8b863224534
Henrique Almeida <henrique_af1@msn.com>
2021-05-18 16:00:25 -0300

    Refactor IsCpfValid function

--- src/utils/IsCpfValid.ts
+++ src/utils/IsCpfValid.ts
@@ -1,26 +1,43 @@
-export default (cpf: string): boolean => {
-  cpf = cpf.replace(/\D/g, '')
+const FACTOR_DIGIT_1 = 10;
+const FACTOR_DIGIT_2 = 11;
+const MAX_DIGITS_1 = 9;
+const MAX_DIGITS_2 = 10;
 
-  let Soma = 0;
-  let Resto;
+export default (cpf: string = ''): boolean => {
+  cpf = extractDigits(cpf);
+  if (isInvalidLength(cpf)) return false;
+  if (isBlocked(cpf)) return false;
+  const digit1 = calculateDigit(cpf, FACTOR_DIGIT_1, MAX_DIGITS_1);
+  const digit2 = calculateDigit(cpf, FACTOR_DIGIT_2, MAX_DIGITS_2);
+  let calculatedCheckDigit = `${digit1}${digit2}`;
+  return getCheckDigit(cpf) == calculatedCheckDigit;
+}
 
-  for (let i=1; i<=9; i++) {
-    Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
+const extractDigits = (cpf: string) => {
+  return cpf.replace(/\D/g, "");
+}
+
+const isInvalidLength = (cpf: string) => {
+  return cpf.length !== 11;
+}
+
+const isBlocked = (cpf: string) => {
+  const [digit1] = cpf;
+  return cpf.split("").every(digit => digit === digit1);
+}
+
+const calculateDigit = (cpf: string, factor: number, max: number) => {
+  let total = 0;
+  for (const digit of toDigitArray(cpf).slice(0, max)) {
+    total += digit * factor--;
   }
+  return (total%11 < 2) ? 0 : (11 - total%11);
+}
 
-  Resto = (Soma * 10) % 11;
-  if ((Resto == 10) || (Resto == 11)) Resto = 0;
+const toDigitArray = (cpf: string) => {
+  return [...cpf].map(digit => parseInt(digit));
+}
 
-  if (Resto != parseInt(cpf.substring(9, 10))) return false;
-
-  Soma = 0;
-  for (let i = 1; i <= 10; i++){
-    Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
-  }
-
-  Resto = (Soma * 10) % 11;
-
-  if ((Resto == 10) || (Resto == 11)) Resto = 0;
-  if (Resto != parseInt(cpf.substring(10, 11))) return false;
-  return true;
+const getCheckDigit = (cpf: string) => {
+  return cpf.slice(9);
 }
