1e49800886e5b66dea8bf072f4c3abbf8dbe8b40
Henrique Almeida <henrique_af1@msn.com>
2021-06-01 14:31:42 -0300

    Create addDays function to DateUtils

--- src/utils/DateUtils.ts
+++ src/utils/DateUtils.ts
@@ -1,3 +1,5 @@
+const DAY_IN_MILLISECONDS = 24*60*60*1000;
+
 export const dateDiffInYears = (biggerDate: Date, smallDate: Date): number => {
   const ageDifMs = biggerDate.getTime() - smallDate.getTime();
   const ageDate = new Date(ageDifMs);
@@ -8,5 +10,9 @@ export const dateDiffInYears = (biggerDate: Date, smallDate: Date): number => {
 export const yearsAgo = (value: number): Date => {
   const currentDate = new Date();
 
-  return new Date(currentDate.getFullYear() - value, currentDate.getMonth(), currentDate.getDay());
+  return new Date(currentDate.getFullYear() - value, currentDate.getMonth(), currentDate.getDay() - 1);
+};
+
+export const addDays = (date: Date, days: number): Date => {
+  return new Date(date.getTime() + days * DAY_IN_MILLISECONDS);
 };
